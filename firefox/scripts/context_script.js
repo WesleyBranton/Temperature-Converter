89935c012e0fe4ea1a5fca5c18954ed766ff55dc
Wesley Branton <addons@wesleybranton.com>
2019-05-31 18:05:49 -0400

    Added license headers

--- firefox/scripts/context_script.js
+++ firefox/scripts/context_script.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 // Checks that temperature hasn't already been converted
 function convertVerify() {
 	var convertVerify = document.getElementsByClassName('firefoxtempconvertedcomplete');
