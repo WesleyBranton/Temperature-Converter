303659bf38b48014b8b0cf62729b97211c1507d8
Wesley Branton <addons@wesleybranton.com>
2019-08-30 16:57:55 -0400

    Added framework for #1

---
+++ firefox/scripts/content_script.js
@@ -0,0 +1,19 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+function convertTemps() {
+	var temps = document.getElementsByClassName('firefoxtempconvertedcomplete');
+	for (i = 0; i < temps.length; i++) {
+		temps[i].textContent += ' (CONVERTED)';
+	}
+}
+
+var context = document.body;
+var instance = new Mark(context);
+var options = {
+	"element": "span",
+	"className": "firefoxtempconvertedcomplete"
+};
+instance.markRegExp(/-?\d*\.?\d+\s?\°\s?(C|F|c|f)/, options);
+convertTemps();
