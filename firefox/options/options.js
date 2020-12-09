992e7ac660c5b8544d947491b267f2a6c8718e8c
Wesley Branton <addons@wesleybranton.com>
2020-12-09 15:04:24 -0500

    Disable advanced setting if regular setting is not set (#9)

--- firefox/options/options.js
+++ firefox/options/options.js
@@ -8,17 +8,28 @@ function saveOptions() {
         allowAuto: toBoolean(document.settings.allowAuto.value),
         allowAutoAdvanced: toBoolean(document.settings.allowAutoAdvanced.value)
     });
+    updateUI();
 }
 
 // Load settings from storage
 function restoreOptions(item) {
     document.settings.allowAuto.value = item.allowAuto;
     document.settings.allowAutoAdvanced.value = item.allowAutoAdvanced;
+    updateUI();
 }
 
 // Convert string to boolean
 function toBoolean(string) {
     return string == 'true';
+}
+
+// Updates the UI based on the user settings
+function updateUI() {
+    const allowAutoEnabled = toBoolean(document.settings.allowAuto.value);
+    for (i = 0; i < document.settings.allowAutoAdvanced.length; i++) {
+        document.settings.allowAutoAdvanced[i].disabled = !allowAutoEnabled;
+        document.settings.allowAutoAdvanced[i].title = (!allowAutoEnabled) ? "Automatically convert temperatures must be allowed" : "";
+    }
 }
 
 browser.storage.local.get(['allowAuto', 'allowAutoAdvanced'], restoreOptions);
